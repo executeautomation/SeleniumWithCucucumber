920dd5842ffa20ea7773b032771c25748781157a
nadirrkhan <32949786+nadirrkhan@users.noreply.github.com>
2017-10-26 15:14:18 +0500

    update path  of chrome driver
    update path  of chrome driver

--- src/test/java/steps/Hook.java
+++ src/test/java/steps/Hook.java
@@ -22,14 +22,14 @@ public class Hook extends BaseUtil{
     @Before
     public void InitializeTest() {
 
-        System.out.println("Opening the browser : Firefox");
+        System.out.println("Opening the browser : Chrome");
 
         /*System.setProperty("webdriver.firefox.marionette", "D:\\Libs\\geckodriver.exe");
         base.Driver = new FirefoxDriver();*/
 
 
         //Chrome driver
-        System.setProperty("webdriver.chrome.driver", "C:\\Libs\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\Libs\chromedriver.exe");
         base.Driver = new ChromeDriver();
     }
 
