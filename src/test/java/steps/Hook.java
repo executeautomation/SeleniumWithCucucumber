c11377c1e1d78a86aeaa6c2f434aebda58a9a848
JappSingh <44144082+JappSingh@users.noreply.github.com>
2018-10-14 13:56:57 -0500

    Update Hook.java
    For Driver on MAcOS

--- src/test/java/steps/Hook.java
+++ src/test/java/steps/Hook.java
@@ -24,12 +24,12 @@ public class Hook extends BaseUtil{
 
         System.out.println("Opening the browser : Firefox");
 
-        /*System.setProperty("webdriver.firefox.marionette", "D:\\Libs\\geckodriver.exe");
+        /*System.setProperty("webdriver.firefox.marionette", "/Library/WebDrivers/geckodriver 2");
         base.Driver = new FirefoxDriver();*/
 
 
         //Chrome driver
-        System.setProperty("webdriver.chrome.driver", "C:\\Libs\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "/Library/WebDrivers/chromedriver");
         base.Driver = new ChromeDriver();
     }
 
