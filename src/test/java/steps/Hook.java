1cff7c35ad8f99f1acb35dc6da9d65f72be80037
krishna493 <68034240+krishna493@users.noreply.github.com>
2020-07-08 18:02:49 -0400

    Update Hook.java
    Update google path location

--- src/test/java/steps/Hook.java
+++ src/test/java/steps/Hook.java
@@ -43,7 +43,7 @@ public class Hook extends BaseUtil{
 //        System.setProperty("webdriver.chrome.driver", "/Users/karthikkk/ChromeDriver/chromedriver");
 //        base.Driver = new ChromeDriver();
 
-        System.setProperty("webdriver.chrome.driver", "C:/driver/Web/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe");
         base.Driver = new ChromeDriver();
     }
 
