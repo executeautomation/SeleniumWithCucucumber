7ff55ceb4a4f279d99f201be07265bdf30e985da
ExecuteAutomation <kartmcad@gmail.com>
2021-04-02 15:42:42 +1300

    Update TestRunner.java

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -9,7 +9,7 @@ import org.testng.annotations.DataProvider;
  * Created by Karthik on 21/09/2019.
  */
 
-@CucumberOptions(features = {"src/test/java/features"} , plugin = {"json:target/cucumber.json", "pretty:target/report.html"},
+@CucumberOptions(features = {"src/test/java/features"} , plugin = {"json:target/cucumber.json", "pretty"},
         glue = "steps")
 public class TestRunner extends AbstractTestNGCucumberTests {
 
